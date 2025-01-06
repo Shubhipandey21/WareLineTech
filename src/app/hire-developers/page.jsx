f0cb2b096bfb02e599489bcaa3efa04746b18712
unknown <shubhijbm@gmail.com>
2025-01-06 21:42:50 +0530

    ..

--- src/app/hire-developers/page.jsx
+++ src/app/hire-developers/page.jsx
@@ -1,3 +1,4 @@
+"use-client";
 import HeroSection from "@/components/HeroSection/HeroSection"
 import ScheduleCall from "@/components/ScheduleCall/ScheduleCall"
 import BenefitsSection from "@/components/Benefit/BenefitSection"
