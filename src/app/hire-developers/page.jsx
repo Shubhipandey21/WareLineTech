882391b605fab66919857be5edb421a884d401c2
unknown <shubhijbm@gmail.com>
2025-01-06 21:59:22 +0530

    ...

--- src/app/hire-developers/page.jsx
+++ src/app/hire-developers/page.jsx
@@ -1,23 +1,21 @@
-"use-client";
-import HeroSection from "@/components/HeroSection/HeroSection"
-import ScheduleCall from "@/components/ScheduleCall/ScheduleCall"
-import BenefitsSection from "@/components/Benefit/BenefitSection"
-import DevTeam from "@/components/DevTeam/DevTeam"
-import ComparisonTable from "@/components/ComparisonTable/ComparisonTable"
-import ContactUsSection from "@/components/ContactUs/ContactUsSection"
+"use client";
+
+import dynamic from "next/dynamic";
+import HeroSection from "@/components/HeroSection/HeroSection";
+import ScheduleCall from "@/components/ScheduleCall/ScheduleCall";
+import BenefitsSection from "@/components/Benefit/BenefitSection";
+import DevTeam from "@/components/DevTeam/DevTeam";
+import ComparisonTable from "@/components/ComparisonTable/ComparisonTable";
+import ContactUsSection from "@/components/ContactUs/ContactUsSection";
+
+const HireHeroSection = dynamic(() => import('@/components/HeroSection/HeroSection'), {
+  ssr: false, // Disable server-side rendering for this component
+});
+
 export default function HireDevelopers() {
-  // Wrap any client-side logic in the `typeof window` check
-  const floatingImages =
-    typeof window !== "undefined"
-      ? [
-          "https://cdn-ilajckf.nitrocdn.com/utLabjbGVjpaYDQkazoKnooguTzYeQRR/assets/images/optimized/rev-b8cf3a0/tamediacdn.techaheadcorp.com/wp-content/uploads/2024/07/29054608/Group-16898@2x.png",
-          "https://cdn-ilajckf.nitrocdn.com/utLabjbGVjpaYDQkazoKnooguTzYeQRR/assets/images/optimized/rev-b8cf3a0/tamediacdn.techaheadcorp.com/wp-content/uploads/2024/07/29054607/Group-16897@2x.png",
-          "https://cdn-ilajckf.nitrocdn.com/utLabjbGVjpaYDQkazoKnooguTzYeQRR/assets/images/optimized/rev-b8cf3a0/tamediacdn.techaheadcorp.com/wp-content/uploads/2024/07/29054605/Group-16896@2x.png",
-        ]
-      : [];
-    return (
-        <>
-     <HeroSection
+  return (
+    <>
+      <HireHeroSection
         bgImage="https://cdn-ilajckf.nitrocdn.com/utLabjbGVjpaYDQkazoKnooguTzYeQRR/assets/images/optimized/rev-b8cf3a0/tamediacdn.techaheadcorp.com/wp-content/uploads/2024/10/15052418/bg@2x.png"
         buttonColor="bg-black"
         buttonText="Get Started"
@@ -32,20 +30,19 @@ export default function HireDevelopers() {
         ]}
       />
       <DevTeam />
-        <ScheduleCall />
-        <ComparisonTable />
-        <BenefitsSection />
-        <ContactUsSection
-          title="Get in Touch"
-          subtitle="We'd be happy to assist you!"
-          description="Contact us for any questions or inquiries about our cloud solutions."
-          buttonText="Contact Now"
-          highlightText="right now!"
-          highlightGradient={{ from: "#00c6ff", to: "#0072ff" }}
-          buttonGradientFrom="#00c6ff"
-          buttonGradientTo="#0072ff"
-        />
+      <ScheduleCall />
+      <ComparisonTable />
+      <BenefitsSection />
+      <ContactUsSection
+        title="Get in Touch"
+        subtitle="We'd be happy to assist you!"
+        description="Contact us for any questions or inquiries about our cloud solutions."
+        buttonText="Contact Now"
+        highlightText="right now!"
+        highlightGradient={{ from: "#00c6ff", to: "#0072ff" }}
+        buttonGradientFrom="#00c6ff"
+        buttonGradientTo="#0072ff"
+      />
     </>
-    )
-
+  );
 }
