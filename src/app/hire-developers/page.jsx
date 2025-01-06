e1627a88e2a8134d03dfec08047c1676dc8f1fc7
unknown <shubhijbm@gmail.com>
2025-01-06 22:05:35 +0530

    ...

--- src/app/hire-developers/page.jsx
+++ src/app/hire-developers/page.jsx
@@ -1,5 +1,5 @@
 "use client";
-
+import React, { useState } from 'react';
 import dynamic from "next/dynamic";
 import HeroSection from "@/components/HeroSection/HeroSection";
 import ScheduleCall from "@/components/ScheduleCall/ScheduleCall";
